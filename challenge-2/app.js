d7f50a5dd66382f0606fb102ff38ce3bc920c261
Ayushgithubcoder <ayushsharma1a2b@gmail.com>
2025-03-03 01:35:22 +0530

    Challenge-2 completed

--- challenge-2/app.js
+++ challenge-2/app.js
@@ -1,3 +1,34 @@
 /**
  * Write your challenge solution here
  */
+const red= document.querySelector("#redButton")
+
+const green= document.querySelector("#greenButton")
+
+const blue= document.querySelector("#blueButton")
+
+const purple= document.querySelector("#purpleButton")
+
+const reset= document.querySelector("#resetButton")
+
+const color_change=document.querySelector("#mainHeading")
+
+
+red.addEventListener("click",()=>{
+    color_change.style.color="red"
+})
+green.addEventListener("click",()=>{
+    color_change.style.color="green"
+    
+})
+blue.addEventListener("click",()=>{
+    color_change.style.color="blue"
+    
+})
+purple.addEventListener("click",()=>{
+    color_change.style.color="purple"
+})
+reset.addEventListener("click",()=>{
+    color_change.style.color="black"
+    
+})
